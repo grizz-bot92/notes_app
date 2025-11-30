32a182b9fb3ac5c19464de3e70f25a3450f62480
grizz-bot92 <bbenoit752@gmail.com>
2025-11-29 18:02:28 -0800

    added newNote and setNote states and created buttons to toggle between important and unimportant notes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,28 @@ import Note from './components/Note'
 
 const App  = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('new note ...')
+  const [showAll, setShowAll] = useState(true)
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   const addNote = (event) =>{
     event.preventDefault()
-    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) =>{
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
     
     
@@ -14,13 +32,20 @@ const App  = (props) => {
   return(
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => (
+        {notesToShow.map(note => (
           <Note key={note.id} note={note}/>
         ))}
       </ul>
       <form onSubmit={addNote}>
-        <input />
+        <input value={newNote}
+        onChange={handleNoteChange}/>
+
         <button type="submit">save</button>
       </form>
     </div>
